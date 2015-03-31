d1c5ade92e173a7b32744d46eb29a7e165b379c6
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-31 23:32:02 +0000

    edit

--- task_01.py
+++ task_01.py
@@ -2,15 +2,26 @@
 # -*- coding: utf-8 -*-
 """Task_01."""
 
-#from math import ceil
 
-def get_party_stats(families, table_size):
-    """
+def get_party_stats(families, table_size=6):
+    """Function takes two parameters and return and return a headcount for
+        caterer and total number of tables needed.
+        Arg:
+            families(str): list of families which are themselves list of
+                            members.
+            table_size(int): maximum no. of seats at each table.
+        Return:
+               returns the total no. of guests and total tables needed.
+        Examples:
+            get_party_stats([['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack',
+            'Janis']])
+            (6, 3)
     """
     total_guests = 0
     total_tables = 0
     for people in families:
-        total_guests += len(people)
-        tables = (-(-len(people)//table_size))
-        total_tables += tables
-    print (total_guests, total_tables)
+        family = len(people)
+        total_guests += family
+        total_tables += -(-family//table_size)
+
+    return (total_guests, total_tables)
