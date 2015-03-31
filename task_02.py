2b6f0c1bc1fcd2ecf032e579797ca85bb59f191d
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-31 23:33:29 +0000

    edits

--- task_02.py
+++ task_02.py
@@ -3,21 +3,25 @@
 """Task_02."""
 
 
-
-
 def prepare_email(appointments):
-    """
+    """Function takes client's name and date as input and return the
+        email template formatted.
+        Arg:
+            appointments(mix): Input value of a list containing name and
+                                date.
+        Return:
+                returns an email template formatted for the input names
+                and dates.
+        Example:
+                >>> prepare_email([('Jack', 2015), ('Max', 'March 3')])
+                ['Dear Jack, \nI look forward to meeting you on 2015. \nBest,
+                    \nMe',
+                'Dear Max, \nI look forward to meeting you on March 3. \nBest,
+                \nMe']
     """
     email = []
-
-    
-    LIST = 'Dear {}, n\I look forward to meet you on {} \nBest, \n Gp'
-   
-    #counter = 0
+    list1 = 'Dear {}, \nI look forward to meeting you on {}. \nBest, \nMe'
     for item in appointments:
-        #counter += 1
-        name = item[0]
-        date = item[1]
-        
-        email.append(LIST.format(name, date))
-        print LIST.format(name, date)
+        email.append(list1.format(item[0], item[1]))
+
+    return email
